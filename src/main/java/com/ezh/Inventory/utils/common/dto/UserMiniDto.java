e3484216b6137ae3e0b41054c19d8627f5167c01
Heeneth12 <heeneth123@gmail.com>
2026-02-15 20:17:46 +0530

    feat: added getBulkUserDetails api for UserMiniDto in Map

---
+++ src/main/java/com/ezh/Inventory/utils/common/dto/UserMiniDto.java
@@ -0,0 +1,19 @@
+package com.ezh.Inventory.utils.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserMiniDto {
+    private Long id;
+    private String userType;
+    private String UserUuid;
+    private String name;
+    private String email;
+    private String phone;
+}
